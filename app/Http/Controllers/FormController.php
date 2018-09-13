d91b8d941df13485d26794aa992b094529bb116d
Jonathan Bareket <jonathan.bareket@mgb20041-imac27.ber.s-f.com>
2018-09-13 18:00:17 +0200

    form controller changes

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use  Illuminate\Http\RedirectResponse;
-// use Illuminate\Support\Facades\Redirect;
 use App\Form;
 use Storage;
 
@@ -14,17 +13,13 @@ class FormController extends Controller
 
   function append(Request $request) {
     try {
+      
       $origList = Storage::disk('local')->exists('user-details.json') ? json_decode(Storage::disk('local')->get('user-details.json')) : [];
       $inputData = $request->only([ 'username', 'password','checkbox']);
-
       array_push($origList,$inputData);
-
       Storage::disk('local')->put('user-details.json', json_encode($origList, JSON_PRETTY_PRINT));
-
       $safeData = $request->only([ 'username', 'checkbox']);
       return $safeData;
-      //This redirect is not working
-      // return redirect('/')->with($inputData);
 
     } catch(Exception $e) {
 
